b305c9ae58072896822dfaf25bd8e22287d97b11
Valencia Mckinney <vmckinney012@gmail.com>
2019-10-30 12:07:52 -0700

    changed build

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,7 +2,7 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("/Users/unloopunloop/workspace/portfolio/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": () => import("/Users/unloopunloop/workspace/portfolio/node_modules/gatsby-plugin-offline/app-shell.js" /* webpackChunkName: "component---node-modules-gatsby-plugin-offline-app-shell-js" */),
   "component---src-pages-404-js": () => import("/Users/unloopunloop/workspace/portfolio/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-index-js": () => import("/Users/unloopunloop/workspace/portfolio/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
 }
